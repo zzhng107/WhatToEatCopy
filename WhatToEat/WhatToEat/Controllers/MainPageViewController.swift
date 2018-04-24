bc4cb530780a544466108bb58eb378456f9a85c9
Zhiwei Zhang <zzw408673530@gmail.com>
2018-04-24 00:44:26 -0500

    sidebar commit

--- WhatToEat/WhatToEat/Controllers/MainPageViewController.swift
+++ WhatToEat/WhatToEat/Controllers/MainPageViewController.swift
@@ -8,75 +8,130 @@
 
 import Koloda
 
-private var numberOfCards: Int = 8
+fileprivate var dataSource: [UIImage] = []
+private var numberOfCards: Int = 20
+
+private var imgList: [String] = []
 
 class MyKolodaViewController: UIViewController {
     
+    @IBOutlet weak var sideBar: UIStackView!
     @IBOutlet weak var kolodaView: KolodaView!
+    
+    @IBOutlet weak var go: UIButton!
     @IBOutlet weak var like: UIButton!
+    @IBOutlet weak var dislike: UIButton!
     @IBAction func likeAction(_ sender: Any) {
         kolodaView?.swipe(.right)
     }
-    @IBOutlet weak var go: UIButton!
-//    go.setImage(UIImage(named: "nameOfImage.png"), forState: UIControlState.Normal)
-
-    @IBOutlet weak var dislike: UIButton!
     @IBAction func dislikeAction(_ sender: Any) {
         kolodaView?.swipe(.left)
     }
     
+    var sideBarShow = false
+    @IBOutlet weak var sideBarLeading: NSLayoutConstraint!
+    @IBAction func sideBarButton(_ sender: Any) {
+        sideBarShow = !sideBarShow
+        if sideBarShow {
+            sideBarLeading.constant = 0
+        }else{
+            sideBarLeading.constant = -175
+        }
+        UIView.animate(withDuration: 0.2, delay: 0.0, options: .curveEaseIn, animations:{
+            self.view.layoutIfNeeded()
+        })
+    }
     
-    fileprivate var dataSource: [UIImage] = {
-        var array: [UIImage] = []
-        let urls = [URL(string: "https://i.ndtvimg.com/i/2016-06/chinese-625_625x350_81466064119.jpg"),
-                    URL(string: "https://www.seriouseats.com/recipes/images/2017/06/20170617-bulgogi-burger-matt-clifton-1-1500x1125.jpg"),
-                    URL(string: "https://cdn-images-1.medium.com/max/1600/1*xoMh1motSFtZ5yodvEeEYA.jpeg"),
-                    URL(string: "https://cdn.vox-cdn.com/uploads/chorus_image/image/49266033/eatersea0416_mendozas_market_yelp_mendozas_m.0.0.jpg"),
-                    URL(string: "https://i.ndtvimg.com/i/2016-06/chinese-625_625x350_81466064119.jpg"),
-                    URL(string: "https://www.seriouseats.com/recipes/images/2017/06/20170617-bulgogi-burger-matt-clifton-1-1500x1125.jpg"),
-                    URL(string: "https://cdn-images-1.medium.com/max/1600/1*xoMh1motSFtZ5yodvEeEYA.jpeg"),
-                    URL(string: "https://cdn.vox-cdn.com/uploads/chorus_image/image/49266033/eatersea0416_mendozas_market_yelp_mendozas_m.0.0.jpg"),
-                    URL(string: "http://i.imgur.com/w5rkSIj.jpg")]
-        
-        for index in 0..<numberOfCards {
-            
-            let url = urls[index]
-            let data = try? Data(contentsOf: url!)
-            
-            if let imageData = data {
-                let image = UIImage(data: imageData)
-                array.append(image!)
-            }
-            
-//            array.append(UIImage(named: "Card_like_\(index + 1)")!)
+    
+    
+    
+    
+    
+    func loadDishes(_ urlString: String, withCompletion completion: @escaping ()->()) {
+        let url = URL(string: urlString)!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        let userId = [
+            "userId": "5a2dbb53af64cc0021027198",
+            ]
+        do{
+            request.httpBody = try JSONSerialization.data(withJSONObject: userId, options: .prettyPrinted)
+        }catch{
+            print("error in first catch")
         }
         
-        return array
-    }()
+        let task = URLSession.shared.dataTask(with: request as URLRequest) { data, response, error in
+            guard let data = data, error == nil else{
+                print("error=\(error)")
+                return
+            }
+            
+            do{
+                if let output = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? [String: AnyObject] {
+                    for (_, val) in (output["dishes"] as? [String: AnyObject])!{
+                        imgList.append(val["imgUrl"] as! String)
+                    }
+                    completion()
+                }
+            }catch{
+                print("error in second catch")
+            }
+        }
+        task.resume()
+        
+    }
     
     // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        kolodaView.dataSource = self
-        kolodaView.delegate = self
         
         self.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
+        
+        
+        DispatchQueue.global(qos: .background).async {
+            
+            
+            DispatchQueue.main.async {
+                // Run UI Updates or call completion block
+            }
+        }
+        
+        loadDishes("https://us-central1-whattoeat-9712f.cloudfunctions.net/dishes"){()->() in
+            var array: [UIImage] = []
+            
+            let urls = imgList.map({
+                (url: String) -> URL in
+                return URL(string: url)!
+            })
+            
+            for index in 0..<numberOfCards {
+                
+                let url = urls[index]
+                let data = try? Data(contentsOf: url)
+                
+                if let imageData = data {
+                    let image = UIImage(data: imageData)
+                    array.append(image!)
+                }
+            }
+            dataSource = array
+            print(dataSource)
+            
+            self.kolodaView.dataSource = self
+            self.kolodaView.delegate = self
+        }
+        
+        
+        
+        
     }
     
     
-//    // MARK: IBActions
-//    @IBAction func leftButtonTapped() {
-//        kolodaView?.swipe(.left)
-//    }
-//
-//    @IBAction func rightButtonTapped() {
-//        kolodaView?.swipe(.right)
-//    }
-//
-//    @IBAction func undoButtonTapped() {
-//        kolodaView?.revertAction()
-//    }
+    
+    
+    
 }
 
 
@@ -102,14 +157,17 @@ extension MyKolodaViewController: KolodaViewDataSource {
     
     func koloda(_ koloda: KolodaView, viewForCardAt index: Int) -> UIView {
         let out = UIImageView(image: dataSource[Int(index)])
+        
         out.layer.cornerRadius = 8.0
         out.clipsToBounds = true
         out.contentMode = .scaleAspectFill;
-
+        
+        print("img loaded")
+        
         return out
     }
     
-//    func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
-//        return Bundle.main.loadNibNamed("OverlayView", owner: self, options: nil)?[0] as? OverlayView
-//    }
+    //    func koloda(_ koloda: KolodaView, viewForCardOverlayAt index: Int) -> OverlayView? {
+    //        return Bundle.main.loadNibNamed("OverlayView", owner: self, options: nil)?[0] as? OverlayView
+    //    }
 }
